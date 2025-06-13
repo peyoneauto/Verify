46a9ebbaccadfb476eb4027205336a2489b86297
peyoneauto <160504352+peyoneauto@users.noreply.github.com>
2025-06-13 20:23:46 +0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  bot: {
+    owners: ["" , ""],// owners id
+    botID: "",// bot id
+    GuildId: "", // Your Server Id
+    ClientId: "", // Clietn Id
+    serverinvte: "", // Server invite url
+    clientSECRET: "", // Client Secret
+    callbackURL: "", // Call Back Url
+    inviteBotUrl: "",
+    TOKEN: (process.env.token) // Bot Token
+  },
+  website: {
+    PORT: "3004",//website port
+  }
+}
