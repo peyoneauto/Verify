41104490e5c012561f82c266b20f40095f2b7b13
peyoneauto <160504352+peyoneauto@users.noreply.github.com>
2025-06-20 18:45:47 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports = {
   bot: {
-    owners: ["" , ""],// owners id
-    botID: "",// bot id
-    GuildId: "", // Your Server Id
-    ClientId: "", // Clietn Id
-    serverinvte: "", // Server invite url
-    clientSECRET: "", // Client Secret
-    callbackURL: "", // Call Back Url
-    inviteBotUrl: "",
+    owners: ["863474864115023902" , "848794165537800193"],// owners id
+    botID: "1383106052010147840",// bot id
+    GuildId: "1068938684554018836", // Your Server Id
+    ClientId: "1106230290977914920", // Clietn Id
+    serverinvte: "https://discord.gg/jvctnbt3GW", // Server invite url
+    clientSECRET: "GAaAi6CLAmqoa9lNGKo4eGLo87a08rys", // Client Secret
+    callbackURL: "https://coffee-silken-century.glitch.me/login", // Call Back Url
+    inviteBotUrl: "https://discord.com/oauth2/authorize?client_id=1383106052010147840&permissions=8&integration_type=0&scope=bot",
     TOKEN: (process.env.token) // Bot Token
   },
   website: {
