bdf8e49cbda0061c1eec7ae7869d1fe4bee00a58
peyoneauto <160504352+peyoneauto@users.noreply.github.com>
2025-06-20 19:00:28 +0400

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
     ClientId: "1106230290977914920", // Clietn Id
     serverinvte: "https://discord.gg/jvctnbt3GW", // Server invite url
     clientSECRET: "GAaAi6CLAmqoa9lNGKo4eGLo87a08rys", // Client Secret
-    callbackURL: "https://coffee-silken-century.glitch.me/login", // Call Back Url
+    callbackURL: "https://arabbroad.onrender.com/login", // Call Back Url
     inviteBotUrl: "https://discord.com/oauth2/authorize?client_id=1383106052010147840&permissions=8&integration_type=0&scope=bot",
     TOKEN: (process.env.token) // Bot Token
   },
