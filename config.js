43d5ecf94d7208a798248cf67105c7abe3e5bafb
peyoneauto <160504352+peyoneauto@users.noreply.github.com>
2025-06-23 00:19:14 +0400

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
     ClientId: "1106230290977914920", // Clietn Id
     serverinvte: "https://discord.gg/d5V2TzkJGf", // Server invite url
     clientSECRET: "GAaAi6CLAmqoa9lNGKo4eGLo87a08rys", // Client Secret
-    callbackURL: "https://fdf92c28-45a0-47fc-b4c9-7e55ecc68004-00-3h8csesj5o789.pike.replit.dev/login", // Call Back Url
+    callbackURL: "https://arabbroad.onrender.com/login", // Call Back Url
     inviteBotUrl: "https://discord.com/oauth2/authorize?client_id=1383106052010147840&permissions=8&integration_type=0&scope=bot",
     TOKEN: (process.env.token) // Bot Token
   },
