052f9799b51d222fd1f01127dd375063e8353054
peyoneauto <160504352+peyoneauto@users.noreply.github.com>
2025-06-23 00:03:36 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
   bot: {
-    owners: ["863474864115023902" , "848794165537800193"],// owners id
+    owners: ["848794165537800193" , "863474864115023902"],// owners id
     botID: "1383106052010147840",// bot id
-    GuildId: "1068938684554018836", // Your Server Id
+    GuildId: "1194251660764135454", // Your Server Id
     ClientId: "1106230290977914920", // Clietn Id
-    serverinvte: "https://discord.gg/jvctnbt3GW", // Server invite url
+    serverinvte: "https://discord.gg/d5V2TzkJGf", // Server invite url
     clientSECRET: "GAaAi6CLAmqoa9lNGKo4eGLo87a08rys", // Client Secret
-    callbackURL: "https://arabbroad.onrender.com/login", // Call Back Url
+    callbackURL: "https://fdf92c28-45a0-47fc-b4c9-7e55ecc68004-00-3h8csesj5o789.pike.replit.dev/login", // Call Back Url
     inviteBotUrl: "https://discord.com/oauth2/authorize?client_id=1383106052010147840&permissions=8&integration_type=0&scope=bot",
     TOKEN: (process.env.token) // Bot Token
   },
